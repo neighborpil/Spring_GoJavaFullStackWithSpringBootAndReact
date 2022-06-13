781e2c3ef0f3f7c3eaa5b872288ee66d45519716
Neighborpil <neighborpil@naver.com>
2022-06-13 10:15:20 +0900

    save

--- workspace/restful-web-services/src/main/java/com/neighbopril/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ workspace/restful-web-services/src/main/java/com/neighbopril/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -21,6 +21,28 @@ public class TodoHardcodedService {
     return todos;
   }
 
+  public Todo findById(long id) {
+
+    for (Todo todo : todos) {
+      if (todo.getId() == id) {
+        return todo;
+      }
+    }
+    return null;
+  }
+
+  public Todo save(Todo todo) {
+    if (todo.getId() == -1 || todo.getId() == 0) {
+      todo.setId(++idCounter);
+      todos.add(todo);
+    } else {
+      deleteById(todo.getId());
+      todos.add(todo);
+    }
+    return todo;
+  }
+
+
   public Todo deleteById(long id) {
     Todo todo = findById(id);
     if (todo == null) {
@@ -32,15 +54,6 @@ public class TodoHardcodedService {
     return null;
   }
 
-  public Todo findById(long id) {
-
-    for (Todo todo : todos) {
-      if (todo.getId() == id) {
-        return todo;
-      }
-    }
-    return null;
-  }
 
 
 }
--- workspace/restful-web-services/src/main/java/com/neighbopril/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ workspace/restful-web-services/src/main/java/com/neighbopril/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,14 +1,20 @@
 package com.neighbopril.rest.webservices.restfulwebservices.todo;
 import com.neighbopril.rest.webservices.restfulwebservices.todo.Todo;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -19,9 +25,33 @@ public class TodoResource {
 
   @GetMapping("/users/{username}/todos")
   public List<Todo> getAllTodos(@PathVariable String username) throws InterruptedException{
-    Thread.sleep(3000);
     return todoService.findAll();
 
+  }
+  @GetMapping("/users/{username}/todos/{id}")
+  public Todo getTodo(@PathVariable String username, @PathVariable long id) throws InterruptedException{
+    return todoService.findById(id);
+
+  }
+
+  @PostMapping("/users/{username}/todos")
+  public ResponseEntity<Void> insertTodo(@PathVariable String username, @RequestBody Todo todo) {
+    Todo createdTodo = todoService.save(todo);
+
+    // Location
+    // Get current resource url
+    // {id}
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(createdTodo.getId()).toUri();
+
+    return ResponseEntity.created(uri).build();
+  }
+
+  @PutMapping("/users/{username}/todos/{id}")
+  public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo) {
+    Todo todoUpdated = todoService.save(todo);
+
+    return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
   }
 
   @DeleteMapping("/users/{username}/todos/{id}")
